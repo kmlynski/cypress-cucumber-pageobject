2b84fcd3ad594b5009c6d2a53f2d5c7ce139acb6
Kamil <kamil.mlynski@lottomart.com>
2022-07-18 20:34:17 +0200

    migration to TS

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,16 @@
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const browserify = require("@cypress/browserify-preprocessor");
+const tagify = require("cypress-tags");
 
-module.exports = (on) => {
+module.exports = (on, config) => {
   const options = {
+    ...browserify.defaultOptions,
     typescript: require.resolve("typescript"),
   };
 
-  on("file:preprocessor", cucumber(options));
+  on("file:preprocessor", (file) => {
+    return file.filePath.includes(".feature")
+      ? cucumber(options)(file)
+      : tagify(config)(file);
+  });
 };
